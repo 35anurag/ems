e7cd98dd7a0fff35829d893366f1b2ba28c2efe4
Sandhya KC <12101049@students.koi.edu.au>
2026-04-23 14:06:22 +0930

    Ems system auth and folder structure

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import {SignOutButton} from "@/app/components/sign-out-button"
+const dashboard = () => {
+  return (
+    <div>
+      <p>Dashboard</p>
+      {/* <SignOutButton /> */}
+    </div>
+  )
+}
+
+export default dashboard
